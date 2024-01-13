f72e48a03764d2effc2d0fbb0e2b920fcf97ab89
ValentynStarushok <l1nk3rino@gmail.com>
2024-01-14 00:11:09 +0200

    add settings

---
+++ src/config/settings/prod.py
@@ -0,0 +1,24 @@
+from config.settings.base_settings import *  # NOQA
+
+SECRET_KEY = "django-insecure-k4r%kk1z2pmmeh1+ov$p^)3mchk^4ybb9(!$*(%(j+^anol)ca"
+
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = "static/"
